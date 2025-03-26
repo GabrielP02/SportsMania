ac348228d5ac23ec717a095960212d58cf1eb513
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-03-25 21:48:04 -0300

    Add files via upload

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { APP_ROUTES } from "../utils/constants";
+
+const SignUp = () => {
+
+    return(
+<>
+<h1>Cadastro</h1>
+</>
+    );
+}
+
+export default SignUp;
