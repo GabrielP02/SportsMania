c5f3d3232b9ad77b23da94b34dd5210a501440e1
Andrey Ranielly <76213189+Andrey-Ranielly@users.noreply.github.com>
2025-03-25 23:07:23 -0300

    Update App.jsx
    Adicionando rota para o SearchPage.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { APP_ROUTES } from './utils/constants';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import SearchPage from './pages/SearchPage';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Route exact path="/" element={<Navigate to={APP_ROUTES.SIGN_IN} />} />
     <Route path={APP_ROUTES.SIGN_UP} exact element={<SignUp />} />
     <Route path={APP_ROUTES.SIGN_IN} exact element={<SignIn />} />
+    <Route exact path="/" element={<Navigate to={APP_ROUTES.SEARCH_PAGE} />} />
     </Routes>
     </BrowserRouter>
   )
