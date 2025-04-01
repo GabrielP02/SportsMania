a847180d670524300177f8a973f50c872d196ae1
Andrey Ranielly <76213189+Andrey-Ranielly@users.noreply.github.com>
2025-04-01 10:50:00 -0300

    Create Adress.jsx
    Criando página Endereço.

---
+++ src/pages/Adress.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { APP_ROUTES } from "../utils/constants";
+
+const Adress = () => {
+
+    return(
+<>
+<h1>Endereços</h1>
+
+</>
+    );
+}
+
+export default Adress;
