e32d3cbc2089503f0068c508f35a76474f5793d9
Andrey Ranielly <76213189+Andrey-Ranielly@users.noreply.github.com>
2025-04-07 21:40:37 -0300

    Update Adress.jsx
    Ainda não está funcionando, mas vou dar commit pra analisar o erro com o grupo mais tarde.

--- src/pages/Adress.jsx
+++ src/pages/Adress.jsx
@@ -7,6 +7,13 @@ const Adress = () => {
 <>
 <h1>Endereços</h1>
 
+    <script async src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=Function.prototype&loading=async&libraries=maps,marker&v=beta">
+    </script>
+
+<gmp-map center="-8.022706985473633,-34.91827392578125" zoom="14" map-id="DEMO_MAP_ID">
+    <gmp-advanced-marker position="-8.022706985473633,-34.91827392578125" title="Nossa loja"></gmp-advanced-marker>
+</gmp-map>
+
 </>
     );
 }
