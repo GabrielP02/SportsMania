13829e4b56480a66972cb39b5ad1d704db336373
Andrey Ranielly <76213189+Andrey-Ranielly@users.noreply.github.com>
2025-04-23 02:09:09 -0300

    Update MyOrders.jsx
    Adicionando a NavBar que esqueceram no commit passado.

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 
 const MyOrders = () => {
   return (
+    <>
+    <NavBar />
     <div className="flex items-center justify-center h-[80vh] flex-col">
       <h1 className="text-2xl font-bold">Meus Pedidos</h1>
       <p className="text-lg text-gray-600">
         Aqui você pode ver todos os seus pedidos anteriores.
       </p>
     </div>
+    </>
   );
 };
 
