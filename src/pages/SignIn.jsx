ced7ed7d266dc7d77c8cd52352798f9221789f17
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-05-06 12:37:14 +0000

    update signIn/signUp

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -12,7 +12,7 @@ const SignIn = () => {
     console.log(email);
     console.log(password);
     // Adicione aqui sua lógica de autenticação
-    
+
     // Redireciona para a homepage após o login
     navigate(APP_ROUTES.HOME_PAGE);
   }
@@ -22,8 +22,9 @@ const SignIn = () => {
   }
 
   return (
-    <div className="flex h-screen flex-row-reverse">
-      <div className="bg-yellow-500 rounded-l-[13vw] w-1/3 py-50 items-center">
+    <div className="flex flex-row-reverse items-center h-auto min-h-screen">
+
+      <div className="bg-yellow-500 rounded-l-[13vw] w-1/2 flex flex-col items-center justify-center h-screen gap-8">
         <h1 className="text-center font-normal text-4xl mb-4">
           Seja bem-vindo a
         </h1>
@@ -33,7 +34,7 @@ const SignIn = () => {
         <h3 className="text-center font-normal text-xl mb-4">
           Clique abaixo para se registrar
         </h3>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center w-3/3">
           <button
             className="flex justify-center p-2 rounded-md w-1/3 self-center bg-black hover:bg-neutral-900 text-yellow-300"
             onClick={handleSignUpRedirect}
@@ -44,44 +45,49 @@ const SignIn = () => {
           </button>
         </div>
       </div>
-      
-      <div className="flex flex-1 flex-col justify-evenly items-center">
+
+      <div className="flex flex-col items-center justify-center w-1/2 ">
+
         <h2 className="text-center font-medium text-2xl mb-4">
           Entre na sua conta
         </h2>
-        <form className="space-y-6 w-2/3 items-center" onSubmit={handleLogin}>
-          <div>
-            <input
-              className="bg-gray-300 p-2 rounded-md w-full"
-              type="email"
-              placeholder="E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <input
-              className="bg-gray-300 p-2 rounded-md w-full"
-              type="password"
-              placeholder="Senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <button
-              type="submit"
-              className="flex justify-center p-2 rounded-md w-1/2 self-center bg-yellow-500 hover:bg-yellow-300"
-            >
-              <span className="font-medium">
-                ENTRAR
-              </span>
-            </button>
-          </div>
-        </form>
+        <div className="flex flex-1 flex-col justify-evenly items-center">
+
+          <form className='space-y-8 sm:space-y-6 w-2/3 items-center'>
+            <div>
+              <input
+                className="border-2 outline-none rounded-md w-3/3 "
+                type="email"
+                placeholder="Digite seu E-mail"
+                value={email}
+                required
+                onChange={(e) => { setEmail(e.target.value); }}
+              />
+            </div>
+            <div>
+              <input
+                className="border-2 outline-none rounded-md w-3/3"
+                type="password"
+                placeholder="*******"
+                value={password}
+                required
+                onChange={(e) => { setPassword(e.target.value); }}
+              />
+            </div>
+            <div className="flex flex-col items-center w-3/3">
+              <button
+                className="flex justify-center p-2 rounded-md w-1/2 self-center bg-gray-800  text-white hover:bg-gray-700 hover:scale-102  transition-all duration-150 ease-in-out"
+                onClick={handleLogin}
+              >
+                <span>
+                  Entrar
+                </span>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
+
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,100 +6,103 @@ import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
 
-const[nome,setNome] = useState('');
-const[email,setEmail] = useState('');
-const[password,setPassword] = useState('');
-const navigate = useNavigate();
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-const handleClickEntrar = () => {
+  const handleClickEntrar = () => {
 
     navigate(APP_ROUTES.SIGN_IN)
-}
+  }
 
-const handleRegister = () => {
-     console.log(nome);
-     console.log(email);
-     console.log(password);
-}
+  const handleRegister = () => {
+    console.log(nome);
+    console.log(email);
+    console.log(password);
+  }
 
-return(
-<>
-<div className="flex h-screen">
-<div className="bg-yellow-500 rounded-r-[13vw] w-1/3 py-50 items-center">
-        <h1 className="text-center font-normal text-4xl mb-4 ">
-          Bem-vindo de volta à
-        </h1>
-        <h1 className="text-center font-extrabold text-4xl mb-4 ">
-         SPORTS MANIA
-        </h1>
-        <h3 className="text-center font-normal text-xl   mb-4 ">
-         Acesse sua conta já criada
-        </h3>
-        <div className="flex flex-col items-center">
-              <button 
+  return (
+    <>
+      <div className="flex flex-row-reverse items-center h-auto min-h-screen">
+
+        <div className="bg-yellow-500 rounded-l-[13vw] w-1/2 flex flex-col items-center justify-center h-screen gap-8">
+          <h1 className="text-center font-normal text-4xl mb-4">
+            Seja bem-vindo a
+          </h1>
+          <h1 className="text-center font-extrabold text-4xl mb-4">
+            SPORTS MANIA
+          </h1>
+          <h3 className="text-center font-normal text-xl mb-4">
+            Clique abaixo para se registrar
+          </h3>
+          <div className="flex flex-col items-center w-3/3">
+            <button
               className="flex justify-center p-2 rounded-md w-1/3 self-center bg-black hover:bg-neutral-900 text-yellow-300"
               onClick={handleClickEntrar}
-              >
-                <span className="text-xl font-normal">
-                  ENTRAR
-                </span>
-              </button>
-            </div>
-</div>
-<div className="flex flex-1 flex-col justify-evenly items-center">
-        <h2 className="text-center font-medium text-2xl mb-4 ">
-          Entre na sua conta
-        </h2>
-    <div>
-        <img>
-        </img>
-    </div>
-          <form className="space-y-6 w-2/3 items-center">
-          <div>
-              <input
-                className="bg-gray-300 p-2 rounded-md w-3/3"
-                type="text"
-                placeholder="Nome"
-                value={nome}
-                onChange={(e) => { setNome(e.target.value); }}
-                required
-              />
-            </div>
-            <div>
-              <input
-                className="bg-gray-300 p-2 rounded-md w-3/3"
-                type="email"
-                placeholder="E-mail"
-                value={email}
-                onChange={(e) => { setEmail(e.target.value); }}
-                required
-              />
-            </div>
-            <div>
-              <input
-                className="bg-gray-300 p-2 rounded-md w-3/3"
-                type="password"
-                placeholder="Senha"
-                value={password}
-                onChange={(e) => { setPassword(e.target.value); }}
-                required
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <button
-                className="flex justify-center p-2 rounded-md w-1/2 self-center bg-yellow-500 hover:bg-yellow-300"
-                onClick={handleRegister}
-                >
-                <span className="font-medium">
-                  Registre-se
-                </span>
-              </button>
-            </div>
-          </form>
+            >
+              <span className="text-xl font-normal">
+                REGISTRE-SE
+              </span>
+            </button>
+          </div>
         </div>
-</div>
-</>
-    );
+
+        <div className="h-1/2 shadow-lg rounded-md bg-white p-8 flex flex-col w-2/3 sm:w-1/2">
+
+          <h2 className="text-center font-medium text-2xl mb-4">
+            Entre na sua conta
+          </h2>
+
+          <div className="flex flex-col justify-center items-center w-full">
+            <form className='gap-8 flex flex-col justify-center items-center w-full'>
+              <div>
+                <input
+                  className="border-2 outline-none rounded-md w-3/3"
+                  type="text"
+                  placeholder="Digite seu nome"
+                  value={nome}
+                  required
+                  onChange={(e) => { setNome(e.target.value); }}
+                />
+              </div>
+              <div>
+                <input
+                  className="border-2 outline-none rounded-md w-3/3 "
+                  type="email"
+                  placeholder="Digite seu E-mail"
+                  value={email}
+                  required
+                  onChange={(e) => { setEmail(e.target.value); }}
+                />
+              </div>
+              <div>
+                <input
+                  className="border-2 outline-none rounded-md w-3/3"
+                  type="password"
+                  placeholder="*******"
+                  value={password}
+                  required
+                  onChange={(e) => { setPassword(e.target.value); }}
+                />
+              </div>
+              <div className="flex flex-col items-center w-3/3">
+                <button
+                  className="flex justify-center p-2 rounded-md w-1/2 self-center bg-gray-800  text-white hover:bg-gray-700 hover:scale-102  transition-all duration-150 ease-in-out"
+                  onClick={handleRegister}
+                >
+                  <span>
+                    Entrar
+                  </span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
