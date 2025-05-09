4fe61ffc521836b3af42fb8d212c96ee44eaa7af
Andrey Ranielly <76213189+Andrey-Ranielly@users.noreply.github.com>
2025-05-09 11:54:20 -0300

    Update Adress.jsx

--- src/pages/Adress.jsx
+++ src/pages/Adress.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { APP_ROUTES } from "../utils/constants";
-import '../Styles/Adress.css'; // Importando o CSS para estilização
 import Navbar from "../components/NavBar";
 
 const Adress = () => {
@@ -9,12 +8,12 @@ const Adress = () => {
 <>
 <Navbar />
 
-<script async src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=Function.prototype&loading=async&libraries=maps,marker&v=beta"></script>
+<script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrddWCojVRtJkn1RCAaHX6NpCAgffB4ks&callback=Function.prototype&loading=async&libraries=maps,marker&v=beta"></script>
 
-<div className="Adress-container">
+<div class="Adress-container" className="p-20">
 <h1 className="titulo">Conheça nossa loja física</h1>
 
-<div id="map_canvas" style= {{width:700, height:500}}>
+<div id="map_canvas" style= {{width:700, height:500}}className="m-auto p-5">
 
 <gmp-map center="-8.022706985473633,-34.91827392578125" zoom="17" map-id="DEMO_MAP_ID">
     <gmp-advanced-marker position="-8.022706985473633,-34.91827392578125" title="Nossa loja"></gmp-advanced-marker>
@@ -22,23 +21,26 @@ const Adress = () => {
     
 </div>
 
-<div className="blocos-container">
-<div className="bloco">
-    <p>Sua loja esportiva no coração da cidade! Desfrute da conveniência e acessibilidade de nossa localização privilegiada.</p>
+<div className="flex justify-center bg-amber-300 p-5 mx-100">
+  <div className="flex flex-col md:flex-row justify-center items-start gap-8 max-w-6xl w-full">
+    <div className="p-5 text-lg md:w-1/3 w-full">
+      <p>Sua loja esportiva no coração da cidade! Desfrute da conveniência e acessibilidade de nossa localização privilegiada.</p>
     </div>
 
-    <div className="bloco">
-    <h2><b>Horário de Funcionamento</b></h2>
-    <p>Seg-Sex: 9h às 18h</p>
-    <p>Sábado:  9h às 14h</p>
-    <p>Domingo: Fechado</p>
+    <div className="p-5 text-lg md:w-1/3 w-full">
+      <p><b>Horário de Funcionamento</b></p>
+      <p>Seg-Sex: 9h às 18h</p>
+      <p>Sábado:  9h às 14h</p>
+      <p>Domingo: Fechado</p>
     </div>
 
-    <div className="bloco">
-    <p>Nossa localização facilita o acesso aos recursos e serviços essenciais para uma experiência esportiva completa.</p>
+    <div className="p-5 text-lg md:w-1/3 w-full">
+      <p>Nossa localização facilita o acesso aos recursos e serviços essenciais para uma experiência esportiva completa.</p>
     </div>
-    </div>
-    </div>
+  </div>
+</div>
+
+</div>
 </>
     );
 }
